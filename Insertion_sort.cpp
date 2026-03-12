ac4cda97d9dc36dfbcb4a844d80006f1b9b02ee5
RifkyIkhsan <pratamarifky069-ship-it>
2026-03-12 18:42:06 +0700

    menambahkan program insertion sort

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int arr[20];            //membuat array dengan panjang data 20
 int n;                  //membuat variable inputan n
 
-void input (){
+void input (){  //procedure input
     while (true){
         cout << "Masukkan Jumlah Data Pada Array : ";
         cin >> n;
@@ -27,7 +27,7 @@ void input (){
     }
 }
 
-void insertionsort(){
+void insertionsort(){   //procedure insertionsort
     int temp;
     int j;
 
